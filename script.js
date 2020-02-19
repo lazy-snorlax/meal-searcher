1af88570406f4ef8b06c719b39e22d43b11d68ac
Anthony Morelli <blacknebula236@gmail.com>
2020-02-19 21:45:00 +1030

    Added functionality for the search for a main ingredient

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const search = document.getElementById('search'),
   submit = document.getElementById('submit'),
+  searchIng = document.getElementById('searchIng'),
   random = document.getElementById('random'),
   mealsEl = document.getElementById('meals'),
   resultHeading = document.getElementById('result-heading'),
@@ -60,6 +61,47 @@ function getMealById(id) {
     });
 }
 
+// Filter meal by main ingredient
+function filterMeal(e) {
+  e.preventDefault();
+
+  // Clear single meal
+  single_mealEl.innerHTML = '';
+
+  // Get search term
+  const term = searchIng.value;
+
+  if (term.trim()) {
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${term}`)
+      .then(res => res.json())
+      .then(data => {
+        // console.log(data);
+        resultHeading.innerHTML = `<h2>Search results for '${term}':</h2>`;
+
+        if (data.meals === null) {
+          resultHeading.innerHTML = `<p>There are no search results.</p>`;
+        } else {
+          mealsEl.innerHTML = data.meals
+            .map(
+              meal => `
+            <div class="meal">
+              <img src="${meal.strMealThumb}" alt="${meal.strMeal}"/>
+              <div class="meal-info" data-mealID="${meal.idMeal}">
+                <h3>${meal.strMeal}</h3>
+              </div>
+            </div>
+          `
+            )
+            .join('');
+        }
+      });
+    // Clear search text
+    searchIng.value = '';
+  } else {
+    // alert('Please enter a search term');
+  }
+}
+
 // Get random meal
 function getRandomMeal() {
   // Clear meals and heading
@@ -111,6 +153,7 @@ function addMealDOM(meal) {
 // Event Listeners
 submit.addEventListener('submit', searchMeal);
 random.addEventListener('click', getRandomMeal);
+filter.addEventListener('click', filterMeal);
 
 mealsEl.addEventListener('click', e => {
   const mealInfo = e.path.find(item => {
